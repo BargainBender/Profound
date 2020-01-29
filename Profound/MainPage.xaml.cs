e56cb78b4db4cf5f62c6eb42261fc4ff8e924de5
sebastianmartinez <companionpony88@gmail.com>
2020-01-29 19:13:52 +0800

    Re-programmed deleted LogIn.xaml code.

--- Profound/MainPage.xaml.cs
+++ Profound/MainPage.xaml.cs
@@ -16,6 +16,14 @@ namespace Profound
         public MainPage()
         {
             InitializeComponent();
+
+            entry_studentNumber.Focused += (s,e) => lbl_studentNumber.TextColor = Color.FromHex("#ED4264");
+            entry_studentNumber.Unfocused += (s,e) => lbl_studentNumber.TextColor = Color.FromHex("#333333");
+            entry_studentNumber.Completed += (s,e) => entry_password.Focus();
+
+            entry_password.Focused += (s, e) => lbl_password.TextColor = Color.FromHex("#ED4264");
+            entry_password.Unfocused += (s, e) => lbl_password.TextColor = Color.FromHex("#333333");
+
         }
 
     }
---
+++ Profound/Views/LogIn.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Profound.Views
+{
+    public partial class LogIn : ContentPage
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+
+            entry_studentNumber.Focused += (s, e) => lbl_studentNumber.TextColor = Color.FromHex("#ED4264");
+            entry_studentNumber.Unfocused += (s, e) => lbl_studentNumber.TextColor = Color.FromHex("#333333");
+            entry_studentNumber.Completed += (s, e) => entry_password.Focus();
+
+            entry_password.Focused += (s, e) => lbl_password.TextColor = Color.FromHex("#ED4264");
+            entry_password.Unfocused += (s, e) => lbl_password.TextColor = Color.FromHex("#333333");
+        }
+    }
+}
